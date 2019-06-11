a416293404aba0b7ad0978088a384c94d8fe8595
joeyrobert <joey.robert@adroll.com>
2019-06-11 14:39:29 -0400

    Add concurrently to the downgrade too

--- migrations/00021_single_trigram_index.sql
+++ migrations/00021_single_trigram_index.sql
@@ -15,9 +15,9 @@ CREATE INDEX CONCURRENTLY trgm_idx_jobs ON jobs USING gin (
 -- SQL in this section is executed when the migration is rolled back.
 DROP INDEX trgm_idx_jobs;
 
-CREATE INDEX trgm_idx_jobs_job_id ON jobs USING gin (job_id gin_trgm_ops);
-CREATE INDEX trgm_idx_jobs_job_name ON jobs USING gin (job_name gin_trgm_ops);
-CREATE INDEX trgm_idx_jobs_job_queue ON jobs USING gin (job_queue gin_trgm_ops);
-CREATE INDEX trgm_idx_jobs_image ON jobs USING gin (image gin_trgm_ops);
-CREATE INDEX trgm_idx_jobs_command_line ON jobs USING gin (command_line gin_trgm_ops);
-CREATE INDEX trgm_idx_jobs_job_definition ON jobs USING gin (job_definition gin_trgm_ops);
+CREATE INDEX CONCURRENTLY trgm_idx_jobs_job_id ON jobs USING gin (job_id gin_trgm_ops);
+CREATE INDEX CONCURRENTLY trgm_idx_jobs_job_name ON jobs USING gin (job_name gin_trgm_ops);
+CREATE INDEX CONCURRENTLY trgm_idx_jobs_job_queue ON jobs USING gin (job_queue gin_trgm_ops);
+CREATE INDEX CONCURRENTLY trgm_idx_jobs_image ON jobs USING gin (image gin_trgm_ops);
+CREATE INDEX CONCURRENTLY trgm_idx_jobs_command_line ON jobs USING gin (command_line gin_trgm_ops);
+CREATE INDEX CONCURRENTLY trgm_idx_jobs_job_definition ON jobs USING gin (job_definition gin_trgm_ops);
