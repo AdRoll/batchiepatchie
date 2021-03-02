567560dc85fb98adf9ac818f236ee0dcc1e0ed3b
joeyrobert <joey.robert@adroll.com>
2021-03-01 20:14:18 -0500

    Concurrently

--- migrations/00022_reduced_trigram_index.sql
+++ migrations/00022_reduced_trigram_index.sql
@@ -1,15 +1,15 @@
 -- +goose Up
 -- SQL in this section is executed when the migration is applied.
-DROP INDEX trgm_idx_jobs;
+DROP INDEX CONCURRENTLY trgm_idx_jobs;
 
-CREATE INDEX trgm_idx_jobs ON jobs USING gin (
+CREATE INDEX CONCURRENTLY trgm_idx_jobs ON jobs USING gin (
     (job_id || job_name || job_queue || image) gin_trgm_ops
 );
 
 -- +goose Down
 -- SQL in this section is executed when the migration is rolled back.
-DROP INDEX trgm_idx_jobs;
+DROP INDEX CONCURRENTLY trgm_idx_jobs;
 
-CREATE INDEX trgm_idx_jobs ON jobs USING gin (
+CREATE INDEX CONCURRENTLY trgm_idx_jobs ON jobs USING gin (
     (job_id || job_name || job_queue || image || command_line || job_definition) gin_trgm_ops
 );
