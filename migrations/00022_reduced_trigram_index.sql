f611c884318010e0b6992aee23be52fa2605c8ed
Joey Robert <joey.robert@adroll.com>
2021-03-04 16:28:10 -0500

    Reduced trgm index on jobs table, exclude command_line / job_definition (#39)
    * Reduced trgm index on jobs table, exclude command_line / job_definition
    
    * Concurrently

---
+++ migrations/00022_reduced_trigram_index.sql
@@ -0,0 +1,15 @@
+-- +goose Up
+-- SQL in this section is executed when the migration is applied.
+DROP INDEX CONCURRENTLY trgm_idx_jobs;
+
+CREATE INDEX CONCURRENTLY trgm_idx_jobs ON jobs USING gin (
+    (job_id || job_name || job_queue || image) gin_trgm_ops
+);
+
+-- +goose Down
+-- SQL in this section is executed when the migration is rolled back.
+DROP INDEX CONCURRENTLY trgm_idx_jobs;
+
+CREATE INDEX CONCURRENTLY trgm_idx_jobs ON jobs USING gin (
+    (job_id || job_name || job_queue || image || command_line || job_definition) gin_trgm_ops
+);
