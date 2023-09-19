baf8df5b4e444d1874d72625ddac0f3f42f1e76f
Manuel Garrido <manuel.garrido@carpe.io>
2023-09-19 12:40:53 +0100

    remove unused import

--- fetcher/fetcher.go
+++ fetcher/fetcher.go
@@ -12,7 +12,6 @@ import (
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
-	log "github.com/sirupsen/logrus"
 )
 
 var s3Regex = regexp.MustCompile("^s3://([^/]+)/(.+)$")
